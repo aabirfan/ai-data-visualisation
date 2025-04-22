9faa09204cadb778a031a2c2f302a81282711752
Hans Bekkhus Arneberg <hansa@uia.no>
2025-04-22 13:16:58 +0200

    added some unit tests for extraction cases and some chart building. Added automated unit test on PR

---
+++ src/backend/tests/unit/test_llm.py
@@ -0,0 +1,17 @@
+import pytest
+import sys
+import os
+from unittest.mock import patch, MagicMock
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+
+## IMPORTS
+from models.llm_chart import generate_text_from_query_results
+from models.embeddings import extract_variables
+
+## LLM 
+
+def test_llm_text_error_when_db_query_results_empty():
+    result = generate_text_from_query_results("Test query", [], "ph in")
+    assert "error" in result
+    assert result["error"] == "No data retrieved from MongoDB."
+
